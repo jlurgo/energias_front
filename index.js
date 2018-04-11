0b49232e8aae46712dd74ffd8f7476f6353e8a76
jlurgo <jlurgo@gmail.com>
2018-04-11 11:57:00 -0300

    inicial

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
 $(function(){
-    $("#btn_subirMedicion").click(function(){
-        $.ajax({
-			url: "https://backend-energias.herokuapp.com/guardarMedicion",
-			type: "POST",
-			async: true,
-            dataType: "json",
-            contentType: "application/json",
-			data: JSON.stringify({
-				"Id-MAC": "12.23.2.12.31.32",
-                "mediciones":[
-                    {fecha:"1/5/2017 17:15:20", sensor: "Temperatura-IN", valor: 11.27},
-                    {fecha:"1/5/2017 17:15:22", sensor: "Temperatura-OUT", valor: 15.27},
-                    {fecha:"1/5/2017 17:15:25", sensor: "Caudalimetro", valor: 12.27}                
-                ]
-			})
-        });
-    });    
+    $.ajax({
+        url: "https://backend-energias.herokuapp.com/getMediciones/:10",
+        type: "GET",
+        async: true,
+        dataType: "json",
+        contentType: "application/json",
+        success: function(data){
+            $("#mediciones").text(data);            
+        }
+    }); 
 });
